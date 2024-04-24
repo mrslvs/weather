0725196fe8c915494e0012875936b7cf7752f080
mrslvs <sekerka.miroslav@outlook.com>
2024-04-24 19:55:51 +0200

    add api call

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 
 
 header('Access-Control-Allow-Origin: ' . $_ENV['FRONTEND_URL']); // Adjust origin as needed
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true'); // Optional, enable if sending cookies
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,20 @@ require_once ("./config/cors.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   echo 'ok';
+
 } else {
-  echo __DIR__;
+  $url = 'https://api.openweathermap.org/data/2.5/weather?lat=48&lon=17&appid=' . $_ENV['WEATHER_API_KEY'];
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $response = curl_exec($ch);
+
+  if ($response === false) {
+    echo 'Error: ' . curl_error($ch);
+  } else {
+    // Process the API response
+    echo $response;
+  }
+
+  curl_close($ch);
 }
 ?>
