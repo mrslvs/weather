04916e3ad9868ec604fea49e98057e63dbeca478
mrslvs <sekerka.miroslav@outlook.com>
2024-04-24 20:28:23 +0200

    add database connection

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+$servername = $_ENV["DB_SERVER_NAME"];
+$username = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASSWORD"];
+$dbname = $_ENV["DB_NAME"];
+?>
--- index.php
+++ index.php
@@ -6,6 +6,16 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 require_once ("./config/cors.php");
+require_once ("./config/db.php");
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   echo 'ok';
