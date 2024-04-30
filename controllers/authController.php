8205f415d83874b988be839f88adb2bc059832a3
mrslsvs <sekerka.miroslav@outlook.com>
2024-04-30 21:18:47 +0200

    refactor - remove commentes

--- controllers/authController.php
+++ controllers/authController.php
@@ -1,27 +1,4 @@
 <?php
-// require_once ('../includes/getBodyData.php');
-// require_once ("../models/User.php");    
-// require_once __DIR__ . '/../models/User.php';
-// function isEmailTaken($email, $conn)
-// {
-//     $sql = 'SELECT COUNT(*) FROM user WHERE email = :email';
-//     $stmt = $conn->prepare($sql);
-//     $stmt->bindParam(':email', $email);
-//     $stmt->execute();
-//     $count = $stmt->fetchColumn();
-//     return $count > 0;
-// }
-
-// function isUsernameTaken($username, $conn)
-// {
-//     $sql = 'SELECT COUNT(*) FROM user WHERE username = :username';
-//     $stmt = $conn->prepare($sql);
-//     $stmt->bindParam(':username', $username);
-//     $stmt->execute();
-//     $count = $stmt->fetchColumn();
-//     return $count > 0;
-// }
-
 function register($rawData, $conn)
 {
     $data = json_decode($rawData, true);
@@ -36,22 +13,6 @@ function register($rawData, $conn)
         sendResponse(409, 'Username taken');
         return;
     }
-
-    // $hashed_password = password_hash($data['password'], PASSWORD_DEFAULT);
-
-    // $sql = 'INSERT INTO user (username, email, password) VALUES (:username, :email, :password)';
-    // $stmt = $conn->prepare($sql);
-    // $stmt->bindParam(':username', $data['username']);
-    // $stmt->bindParam(':email', $data['email']);
-    // $stmt->bindParam(':password', $hashed_password);
-
-    // if ($stmt->execute()) {
-    //     // Registration successful
-    //     sendResponse(201, 'Registration successful');
-    // } else {
-        //     // Error occurred while executing the SQL statement
-        //     sendResponse(500, 'Error registering user');
-        // }
         
     if (User::saveUserToDatabase((string) $data['username'], (string) $data['email'], (string) $data['password'], $conn)){
         sendResponse(201, 'Registration successful');
@@ -63,25 +24,6 @@ function register($rawData, $conn)
 
 function login($rawData, $conn){
     $data = json_decode($rawData, true);
-
-    // $sql = 'SELECT * FROM user WHERE username = :username';
-    // $stmt = $conn->prepare($sql);
-    // $stmt->bindParam(':username', $data['username']);
-
-    // if ($stmt->execute()) {
-    //     // Login successful
-    //     if ($stmt->rowCount() > 0) {
-    //         $User = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    //         sendResponse(200, "Login successful and id is: {$userId}");
-    //     }else{
-        
-        //         sendResponse(404, "User error");
-    //     }
-    // } else {
-        //     // Error occurred while executing the SQL statement
-        //     sendResponse(500, 'Error logging in');
-        // }
         
     $User = User::getUserByUsername($data['username'], $conn);
     
@@ -92,6 +34,5 @@ function login($rawData, $conn){
     }else{
         sendResponse(200, "Login successful and id is: {$User->getId()}");
     }
-
 }
 ?>
