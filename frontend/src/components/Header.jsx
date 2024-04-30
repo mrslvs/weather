ed91a49ff6d91f9af1643b162b751391ee123d95
mrslsvs <sekerka.miroslav@outlook.com>
2024-04-30 18:20:09 +0200

    add Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <button value={'Login'} />
+                <button value={'Register'} />
+                <button value={'Dashboard'} />
+                <button value={'Account'} />
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
