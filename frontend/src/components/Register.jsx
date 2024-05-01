609b719e580ce307261c1a0433ddbd92c91586e4
mrslsvs <sekerka.miroslav@outlook.com>
2024-05-01 19:07:36 +0200

    refactor register with react-bootstrap

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,56 +1,73 @@
-import React from "react";
-import axiosInstance from "../api/axiosInstance";
-// import "../assets/styles/index.scss";
+import React from 'react';
+import axiosInstance from '../api/axiosInstance';
+import { Form, Button } from 'react-bootstrap';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const Register = () => {
-  const register = async (e) => {
-    e.preventDefault();
+    const register = async (e) => {
+        e.preventDefault();
 
-    const registerData = {
-      username: document.getElementById("username").value,
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value,
+        const registerData = {
+            username: document.getElementById('username').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+        };
+
+        try {
+            const response = await axiosInstance.post('/register', registerData, {
+                withCredentials: true,
+            });
+            console.log('success');
+            console.log(response);
+        } catch (err) {
+            console.log(err);
+        }
     };
 
-    try {
-      const response = await axiosInstance.post("/register", registerData, {
-        withCredentials: true,
-      });
-      console.log("success");
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return (
-    <form onSubmit={register} className="container">
-      <input
-        type="text"
-        id="username"
-        name="username"
-        placeholder="Enter username"
-        className="form-control mb-3"
-      />
-      <input
-        type="text"
-        id="email"
-        name="email"
-        placeholder="Enter email"
-        className="form-control mb-3"
-      />
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Enter password"
-        className="form-control mb-3"
-      />
-      <button type="submit" className="btn btn-primary">
-        Register
-      </button>
-    </form>
-  );
+    return (
+        // <form onSubmit={register} className="container">
+        <Form onSubmit={register}>
+            {/* <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter username"
+                className="form-control mb-3"
+            /> */}
+            <Form.Group controlId="username" className="mb-2">
+                {/* <Form.Label>Username</Form.Label> */}
+                <Form.Control type="text" placeholder="Enter username" />
+            </Form.Group>
+            <Form.Group controlId="email" className="mb-2">
+                {/* <Form.Label>Email</Form.Label> */}
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="password" className="mb-2">
+                {/* <Form.Label>Password</Form.Label> */}
+                <Form.Control type="password" placeholder="Enter password" />
+            </Form.Group>
+            {/* <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                className="form-control mb-3"
+            />
+            <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter password"
+                className="form-control mb-3"
+            /> */}
+            {/* <button type="submit" className="btn btn-primary">
+                Register
+            </button> */}
+            <Button variant="primary" type="submit">
+                Register
+            </Button>
+        </Form>
+    );
 };
 
 export default Register;
